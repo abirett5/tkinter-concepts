b236d3a0eb59b6ab76c0533e18d8784331b6cbc6
Ashfaque Hossain Abir <108783774+abirett5@users.noreply.github.com>
2024-07-08 22:45:10 +0545

    Updated

--- project-1.py
+++ project-1.py
@@ -36,7 +36,79 @@ from tkinter import *
 
 
 # ---------------------------------------------------------------------------------------------
+# Button - you click it, then it does stuff
 
-window = Tk()
+# count = 0
+#
+#
+# def click():
+#     global count
+#     count += 1
+#     print(count)
+#
+#
+# window = Tk()
+#
+# photo = PhotoImage(file="logo.png")
+# button = Button(window,
+#                 text="Click Me!",
+#                 command=click,
+#                 font=("Comic Sans", 30),
+#                 fg="#00FF00",
+#                 bg="black",
+#                 activeforeground="#00FF00",
+#                 activebackground="black",
+#                 state=ACTIVE,  # DISABLED
+#                 image=photo,
+#                 compound="left")
+# button.pack()
+#
+# window.mainloop()
 
-window.mainloop()
+
+# ---------------------------------------------------------------------------------------------
+# Entry widget - textbox that accepts a single line of user input
+
+
+# def submit():
+#     username = entry.get()
+#     print("Hello " + username)
+#     entry.config(state=DISABLED)
+# 
+# 
+# def delete():
+#     entry.delete(0, END)  # Index 0 to END
+# 
+# 
+# def backspace():
+#     entry.delete(len(entry.get()) - 1, END)
+# 
+# 
+# window = Tk()
+# 
+# entry = Entry(window,
+#               font=("Arial", 50),
+#               fg="#00FF00",
+#               bg="black",
+#               # show="*"  # This property is only for password input
+#               )
+# 
+# entry.insert(0, "Type your name")
+# entry.pack(side=LEFT)
+# 
+# submit_button = Button(window,
+#                        text="submit",
+#                        command=submit)
+# submit_button.pack(side=RIGHT)
+# 
+# delete_button = Button(window,
+#                        text="delete",
+#                        command=delete)
+# delete_button.pack(side=RIGHT)
+# 
+# backspace_button = Button(window,
+#                        text="backspace",
+#                        command=backspace)
+# backspace_button.pack(side=RIGHT)
+# 
+# window.mainloop()
