26aef3f2bf685f7820f9fad696e7b30fcefa8381
Ashfaque Hossain Abir <108783774+abirett5@users.noreply.github.com>
2024-07-08 12:50:00 +0600

    Uploaded

---
+++ project-1.py
@@ -0,0 +1,43 @@
+from tkinter import *
+
+# Widgets = GUI Elements: buttons, textboxes, labels, images
+# Windows = serves as a container to hold or contain these widgets
+
+# window = Tk()  # Instantiate an instance of a window
+# window.geometry("420x420")
+# window.title("Abir Mamur Program")
+# icon = PhotoImage(file="logo.png")
+# window.iconphoto(True, icon)
+# window.config(background="#5cfcff")
+#
+# window.mainloop()  # Place window on computer screen, listen for events
+
+
+# ---------------------------------------------------------------------------------------------
+# Label = an area widget that holds text and/or an image within a window
+# window = Tk()
+# photo = PhotoImage(file="logo.png")
+#
+# label = Label(window,
+#               text="Bro, do you even code?",
+#               font=('Arial', 40, 'bold'),
+#               fg="#00FF00",
+#               bg="black",
+#               relief=RAISED,  # SUNKEN
+#               bd="10",
+#               padx="20",
+#               pady="20",
+#               image=photo,
+#               compound="top")
+# label.pack()
+# --> label.place(x=10, y=10)
+
+# window.mainloop()
+
+
+# ---------------------------------------------------------------------------------------------
+
+window = Tk()
+
+window.mainloop()
+# ==> | Continue from 17:26 | <==
