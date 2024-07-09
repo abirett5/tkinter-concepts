63a44f38b469f6d5e2692aae1464a3953dd88b55
Ashfaque Hossain Abir <108783774+abirett5@users.noreply.github.com>
2024-07-09 11:06:07 +0545

    Updated

--- project-1.py
+++ project-1.py
@@ -74,41 +74,79 @@ from tkinter import *
 #     username = entry.get()
 #     print("Hello " + username)
 #     entry.config(state=DISABLED)
-# 
-# 
+#
+#
 # def delete():
 #     entry.delete(0, END)  # Index 0 to END
-# 
-# 
+#
+#
 # def backspace():
 #     entry.delete(len(entry.get()) - 1, END)
-# 
-# 
+#
+#
 # window = Tk()
-# 
+#
 # entry = Entry(window,
 #               font=("Arial", 50),
 #               fg="#00FF00",
 #               bg="black",
 #               # show="*"  # This property is only for password input
 #               )
-# 
+#
 # entry.insert(0, "Type your name")
 # entry.pack(side=LEFT)
-# 
+#
 # submit_button = Button(window,
 #                        text="submit",
 #                        command=submit)
 # submit_button.pack(side=RIGHT)
-# 
+#
 # delete_button = Button(window,
 #                        text="delete",
 #                        command=delete)
 # delete_button.pack(side=RIGHT)
-# 
+#
 # backspace_button = Button(window,
 #                        text="backspace",
 #                        command=backspace)
 # backspace_button.pack(side=RIGHT)
-# 
+#
+# window.mainloop()
+
+
+# ---------------------------------------------------------------------------------------------
+# Check Button Concepts
+
+
+# def display():
+#     if(x.get() == 1):
+#         print("You agree!")
+#     else:
+#         print("You don't agree :(")
+#
+#
+# window = Tk()
+#
+# x = IntVar()  # BooleanVar(), StringVar()
+#
+# checkBtn_photo = PhotoImage(file="logo.png")
+#
+# check_button = Checkbutton(window,
+#                            text="I agree to something.",
+#                            variable=x,
+#                            onvalue=1,
+#                            offvalue=0,
+#                            command=display,
+#                            font=("Arial", 20),
+#                            fg="#00FF00",
+#                            bg="black",
+#                            activeforeground="#00FF00",
+#                            activebackground="black",
+#                            padx=25,
+#                            pady=10,
+#                            image=checkBtn_photo,
+#                            compound="left")
+#
+#
+# check_button.pack()
 # window.mainloop()
